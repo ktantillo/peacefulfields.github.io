df978f8cb8a5ab3231c57cc8233faea99d8cdaf1
ktantillo <96933702+ktantillo@users.noreply.github.com>
2023-02-14 13:21:31 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SlideInterface } from './imageSlider/types/slide.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  
+}
